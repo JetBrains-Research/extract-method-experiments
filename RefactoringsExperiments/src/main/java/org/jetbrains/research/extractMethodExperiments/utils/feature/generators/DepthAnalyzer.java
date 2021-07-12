180104118d0a547bc8223745c70c09187e32cbfa
Anton Ivanov <vultuc@gmail.com>
2021-07-12 14:46:27 +0300

    refactoring + additional log + doccomments for depthanalyzer

--- RefactoringsExperiments/src/main/java/org/jetbrains/research/extractMethodExperiments/utils/feature/generators/DepthAnalyzer.java
+++ RefactoringsExperiments/src/main/java/org/jetbrains/research/extractMethodExperiments/utils/feature/generators/DepthAnalyzer.java
@@ -3,13 +3,14 @@ package org.jetbrains.research.extractMethodExperiments.utils.feature.generators
 public class DepthAnalyzer {
     public static int[] getNestingDepths(String code) {
         String[] lines = code.split("\n");
-        int depthInLine[] = new int[lines.length];
+        int[] depthInLine = new int[lines.length];
         int currentDepth = 0; // current count
 
         // Traverse the input strings
         for (int i = 0; i < lines.length; i++) {
             for (int j = 0; j < lines[i].length(); j++) {
-                if (lines[i].charAt(j) == '{') break;
+                if (lines[i].charAt(j) == '{')
+                    break;
 
                 if (lines[i].charAt(j) == '}') {
                     currentDepth--;
@@ -25,13 +26,15 @@ public class DepthAnalyzer {
 
     public static int getNestingArea(String code) {
         int area = 0;
-        for (int value : getNestingDepths(code)) area += value;
+        for (int value : getNestingDepths(code))
+            area += value;
         return area;
     }
 
     public static int getNestingDepth(String code) {
         int depth = 0;
-        for (int value : getNestingDepths(code)) depth = Math.max(value, depth);
+        for (int value : getNestingDepths(code))
+            depth = Math.max(value, depth);
         return depth;
     }
 }

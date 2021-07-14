36183fee97c51f226353023d330abf6079aabc2a
Anton Ivanov <vultuc@gmail.com>
2021-07-14 21:49:35 +0300

    fixed haas

--- RefactoringsExperiments/src/main/java/org/jetbrains/research/extractMethodExperiments/csv/models/ICSVItem.java
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.research.extractMethodExperiments.csv.models;
-
-public interface ICSVItem {
-    int getId();
-
-    double getValue();
-
-    void setValue(double newValue);
-}

5fe39416462b44a8d053e2fbe1cfdc9c88da7336
Olenek <vultuc@gmail.com>
2021-10-20 20:11:37 +0300

    script upd

---
+++ generateNegativeDataset.sh
@@ -0,0 +1,11 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+if uname -s | grep -iq cygwin ; then
+    DIR=$(cygpath -w "$DIR")
+    PWD=$(cygpath -w "$PWD")
+fi
+
+for d in $1/*/ ; do # iterating through all directories in $1
+	"$DIR/gradlew" runNegativeRefactorings -PinputProjectPath="$d" -PoutputFilePath="$PWD/out/$(basename $1)/$(basename $d).csv" & sleep 10m;	"$DIR/gradlew" --stop & sleep 10
+done
+
+"$DIR/gradlew" --stop

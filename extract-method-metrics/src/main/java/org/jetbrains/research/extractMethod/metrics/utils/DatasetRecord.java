c368703903fedc659d1bb0aaebc248d385a655cf
Olenek <vultuc@gmail.com>
2022-03-07 20:38:24 +0300

    gson for 'ordered' json objects

--- extract-method-metrics/src/main/java/org/jetbrains/research/extractMethod/metrics/utils/DatasetRecord.java
+++ extract-method-metrics/src/main/java/org/jetbrains/research/extractMethod/metrics/utils/DatasetRecord.java
@@ -4,26 +4,25 @@ import org.jetbrains.research.extractMethod.metrics.features.FeatureItem;
 import org.jetbrains.research.extractMethod.metrics.features.FeaturesVector;
 import org.jetbrains.research.extractMethod.metrics.location.LocationItem;
 import org.jetbrains.research.extractMethod.metrics.location.LocationVector;
-import org.json.JSONObject;
-
+import com.google.gson.JsonObject;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class DatasetRecord {
-    private final JSONObject jsonRecord = new JSONObject();
+    private final JsonObject jsonRecord = new JsonObject();
 
     public DatasetRecord(FeaturesVector featuresVector, LocationVector locationVector,
                          double score, String rawCode) {
         for (FeatureItem item : featuresVector.getItems()) {
-            jsonRecord.put(item.getName(), item.getValue());
+            jsonRecord.addProperty(item.getName(), item.getValue());
         }
 
         for (LocationItem item : locationVector.getItems()) {
-            jsonRecord.put(item.getName(), item.getValue());
+            jsonRecord.addProperty(item.getName(), item.getValue());
         }
 
-        jsonRecord.put("Score", score);
-        jsonRecord.put("Code", rawCode);
+        jsonRecord.addProperty("Score", score);
+        jsonRecord.addProperty("Code", rawCode);
     }
 
     public void writeRecord(FileWriter fw) throws IOException {

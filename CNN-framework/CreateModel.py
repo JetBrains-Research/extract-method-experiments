d3f01a6e8612507232fc7beba333a5b2c58a04d6
Le Nguyen <nguye639@msu.edu>
2022-04-06 15:04:40 -0400

    added model architecture with documentation

--- CNN-framework/CreateModel.py
+++ CNN-framework/CreateModel.py
@@ -1,28 +1,43 @@
 #import libraries 
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers import Dropout, LSTM
-from tensorflow.keras import layers, models
-from keras.layers.convolutional import Conv1D
-from keras.layers.convolutional import MaxPooling1D
+from keras.layers import Dropout, LSTM, Flatten, Dense
+from keras.layers.convolutional import Conv1D, MaxPooling1D
 
-def newModel(layers, drop, dense):
+"""
+This method contains the architecture for the CNN used.
+It uses tensorflow sequential as the basis to crease the model.
+
+@param numConvFilters: number of convolutional filters in the 2nd hidden layer. We recommend >32 to upscale.
+@param dropout: fraction of nodes dropped out of the Max Pooling layer.
+@param numDenseNodes: number of dense nodes in the feed forward layer.
+
+Recommended:
+numConvFilters = 242
+dropout = .215
+numDenseNodes = 190
+
+"""
+def newModel(numConvFilters, dropout, numDenseNodes):
     #Create new sequential model
     model = Sequential()
 
-    #Add layers
+    #Add 1D Convolutional layers
     model.add((Conv1D(filters=32, kernel_size=3, activation='relu')))
-    model.add((Conv1D(filters=i, kernel_size=3, activation='relu')))
+    model.add((Conv1D(filters=numConvFilters, kernel_size=3, activation='relu')))
+    #Add Max Pooling layer
     model.add((MaxPooling1D(pool_size=2)))
-    model.add(Dropout(drop))
+    #Apply dropout
+    model.add(Dropout(dropout))
+    #Flatten model
     model.add(Flatten())
-    model.add(Dense(dense, activation='sigmoid'))
+    #Add fully connected dense layer
+    model.add(Dense(numDenseNodes, activation='sigmoid'))
+    #Add output node
     model.add(Dense(1, activation='sigmoid'))
     
     #Define optimizer
     adam = tf.optimizers.Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
-    #Complie model
+    #Compile model
     model.compile(optimizer=adam, loss='binary_crossentropy', metrics=[tf.keras.metrics.AUC(curve='PR'),                                                                                         tf.keras.metrics.PrecisionAtRecall(0.8)])
     return model
